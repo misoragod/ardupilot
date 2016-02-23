8b86d2e7274f83ae2bf682911288762b0bfaf506
Kazumoto Kojima <kkojima@drone.co.jp>
2016-02-23 14:47:14 +0900

    AP_HAL_Linux: add linux HAL stuff for yatagarasu port

--- libraries/AP_HAL_Linux/GPIO.h
+++ libraries/AP_HAL_Linux/GPIO.h
@@ -38,4 +38,6 @@ private:
 #include "GPIO_Disco.h"
 #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_AERO
 #include "GPIO_Aero.h"
+#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_YATAGARASU
+#include "GPIO_Yatagarasu.h"
 #endif
